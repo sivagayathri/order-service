6834115a6f63fada13ca74b7596d02d343fdf49f
shivagayathri <shivagayathrik2000@gmail.com>
2025-11-07 13:44:36 +0530

    fixed the redis connection issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+      url: redisUrl,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT || 7001);
 }
+
 bootstrap();
